3ac973abea6f02cf528474e752eb62c91f4fb2d6
Rijk van Zanten <rijkvanzanten@me.com>
2019-01-24 18:34:28 -0500

    Register global base components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./app";
 
+// Register all base components as globals
+import "@components/base/";
+
 // Don't warn about using the dev version of Vue in development.
 Vue.config.productionTip = process.env.NODE_ENV === "production";
 
