4cbf84c213bf75b32b563262431b647d809f9695
Rijk van Zanten <rijkvanzanten@me.com>
2019-02-01 12:38:40 -0500

    Add some global reset styles

--- src/app.vue
+++ src/app.vue
@@ -21,4 +21,46 @@ export default {
 
 // Import all the global CSS Custom Properties so they can be used in the components
 @import "@design";
+
+::selection {
+  background: var(--blue-gray-100);
+}
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  line-height: inherit;
+  color: inherit;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+body {
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: var(--font-text);
+  font-weight: 500;
+  line-height: 1.3;
+}
+
+// Get rid of the default button styles
+button {
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  border-radius: 0;
+}
+
+// Don't show the yellow auto-fill color
+input:-webkit-autofill {
+  -webkit-box-shadow: 0 0 0 30px white inset;
+}
 </style>
